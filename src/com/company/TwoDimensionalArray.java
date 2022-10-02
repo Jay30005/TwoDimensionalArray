4c9bc3ef68878e3341c542fd995ac8e7fe1107e8
jarrett Metzgen <jarrettmetzgen@gmail.com>
2022-10-01 18:01:23 -0600

    Initial commit

---
+++ src/com/company/TwoDimensionalArray.java
@@ -0,0 +1,60 @@
+package com.company;
+// Sep 29 (Edited Sep 29)
+        //Suppose the weekly hours for all employees are stored in a two-dimensional array. Each row records an employee’s seven-day work hours with seven columns. Write a program that displays employees and their total hours in increasing order of the total hours.
+
+        //WorkHours.java
+
+        /*
+         * To change this license header, choose License Headers in Project Properties.
+         * To change this template file, choose Tools | Templates
+         * and open the template in the editor.
+
+
+package com.company;
+
+
+/**
+ *
+ *
+ *
+ * Suppose the weekly hours for all employees are stored in a two-dimensional array.
+ * Each row records an employee’s seven-day work hours with seven columns. Write
+ * a program that displays employees and their total hours in increasing order of
+ *  * the total hours.
+ *
+ */
+
+
+       import java.util.Arrays;
+       import java.util.Scanner;
+public class TwoDimensionalArray {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double [][]workHours;
+        double [] totalHours;
+        int numEmployee;
+
+        System.out.println("How many employee are there? ");
+        numEmployee = input.nextInt();
+
+        workHours = new double[numEmployee][7];
+        totalHours = new double[numEmployee];
+
+        for (int row = 0 ; row < workHours.length; row++)
+        {
+            for(int day=0; day < workHours[row].length; day++){
+                System.out.println("Enter the work hour for employee "+ row + "on day " + day);
+                workHours[row][day]= input.nextDouble();
+                totalHours[row] += workHours[row][day];
+            }
+        }
+
+        Arrays.sort(totalHours);
+
+        for (double hours: totalHours)
+        {
+            System.out.print(hours + "  ");
+        }
+    }
+
+}
